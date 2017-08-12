8f38a08ea9304db2fee33507288f7d29193ff340
S_rain <xiaomenxiaomen@qq.com>
2017-08-12 13:07:48 +0800

    How much beers will you drink?
    You have 10 beers. 3 bottles will change another beer. How much beers
    will you drink?

---
+++ src/easy/Beer.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package easy;
+
+/**
+ * @author wangxinyu
+ *
+ */
+public class Beer {
+	public static void main(String[] args) {
+		int beer = 100;
+		int bottole = beer;
+		while(bottole >= 3){
+			beer += bottole/3;
+			bottole = bottole - (bottole/3)*3 + (bottole/3);
+		}
+		System.out.println(beer + " " + bottole);
+	}
+	
+}
