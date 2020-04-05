ba7d0b3182591be9877339b00f5e194c2cdbcaf6
S_rain <xiaomenxiaomen@qq.com>
2020-04-05 20:15:51 +0800

    add some new solves

--- src/easy/Beer.java
+++ src/easy/Beer.java
@@ -8,14 +8,22 @@ package easy;
  *
  */
 public class Beer {
+
+
+public static char getRandomChar() {
+        return (char) (0x4e00 + (int) (Math.random() * (0x9fa5 - 0x4e01 + 1)));
+    }
+
+
 	public static void main(String[] args) {
-		int beer = 100;
-		int bottole = beer;
-		while(bottole >= 3){
-			beer += bottole/3;
-			bottole = bottole - (bottole/3)*3 + (bottole/3);
-		}
-		System.out.println(beer + " " + bottole);
+//		int beer = 100;
+//		int bottole = beer;
+//		while(bottole >= 3){
+//			beer += bottole/3;
+//			bottole = bottole - (bottole/3)*3 + (bottole/3);
+//		}
+//		System.out.println(beer + " " + bottole);
+		System.out.println(getRandomChar());
 	}
 	
 }
