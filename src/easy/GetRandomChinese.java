71372542c86c890a482f6b1a0df26aa3a187fb7c
S_rain <xiaomenxiaomen@qq.com>
2020-04-13 10:46:11 +0800

    fix some problems

---
+++ src/easy/GetRandomChinese.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package easy;
+
+/**
+ * @author wangxinyu
+ *
+ */
+public class GetRandomChinese {
+	public static String getRandomChar() {
+		String s = "";
+		for(int i = 0; i < 10; i++){
+			s = s +  (char) (0x4e00 + (int) (Math.random() * (0x9fa5 - 0x4e01 + 1))) + "";
+		}
+        return  s;
+    }
+	public static void main(String[] args) {
+		System.out.println(getRandomChar());
+	}
+}
