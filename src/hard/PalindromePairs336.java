ada3b603b947d5df3218d4249bcd9878edf2f2f0
S_rain <xiaomenxiaomen@qq.com>
2020-08-06 07:28:58 +0800

    daily update

---
+++ src/hard/PalindromePairs336.java
@@ -0,0 +1,74 @@
+package hard;
+
+import java.util.*;
+
+/**
+ * 给定一组唯一的单词， 找出所有不同 的索引对(i, j)，使得列表中的两个单词， words[i] + words[j] ，可拼接成回文串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["abcd","dcba","lls","s","sssll"]
+ * 输出: [[0,1],[1,0],[3,2],[2,4]]
+ * 解释: 可拼接成的回文串为 ["dcbaabcd","abcddcba","slls","llssssll"]
+ * 示例 2:
+ * <p>
+ * 输入: ["bat","tab","cat"]
+ * 输出: [[0,1],[1,0]]
+ * 解释: 可拼接成的回文串为 ["battab","tabbat"]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PalindromePairs336 {
+    List<String> wordsRev = new ArrayList<>();
+    Map<String, Integer> indices = new HashMap<>();
+
+    public List<List<Integer>> palindromePairs(String[] words) {
+        int n = words.length;
+        for (String word : words) {
+            wordsRev.add(new StringBuffer(word).reverse().toString());
+        }
+        for (int i = 0; i < n; ++i) {
+            indices.put(wordsRev.get(i), i);
+        }
+
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            int m = words[i].length();
+            if (m == 0) {
+                continue;
+            }
+            for (int j = 0; j <= m; j++) {
+                if (isPalindrome(word, j, m - 1)) {
+                    int leftId = findWord(word, 0, j - 1);
+                    if (leftId != -1 && leftId != i) {
+                        ret.add(Arrays.asList(i, leftId));
+                    }
+                }
+                if (j != 0 && isPalindrome(word, 0, j - 1)) {
+                    int rightId = findWord(word, j, m - 1);
+                    if (rightId != -1 && rightId != i) {
+                        ret.add(Arrays.asList(rightId, i));
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    public boolean isPalindrome(String s, int left, int right) {
+        int len = right - left + 1;
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(left + i) != s.charAt(right - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int findWord(String s, int left, int right) {
+        return indices.getOrDefault(s.substring(left, right + 1), -1);
+    }
+}
