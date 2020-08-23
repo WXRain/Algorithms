bc20712625c1d094e29abc10f201316a1b9e8404
S_rain <xiaomenxiaomen@qq.com>
2020-08-24 07:43:04 +0800

    daily update

--- src/easy/RepeatedSubstringPattern459.java
+++ src/easy/RepeatedSubstringPattern459.java
@@ -2,6 +2,6 @@ package easy;
 
 public class RepeatedSubstringPattern459 {
     public boolean repeatedSubstringPattern(String s) {
-        return (s+s).substring(1,s.length()*2-1).indexOf(s)!=-1;
+        return (s + s).substring(1, s.length() * 2 - 1).contains(s);
     }
 }
