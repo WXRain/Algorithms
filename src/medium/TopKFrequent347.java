df3a31738baab3a943689fc3595c6ddc086c99a1
S_rain <xiaomenxiaomen@qq.com>
2020-09-07 10:17:53 +0800

    daily update

---
+++ src/medium/TopKFrequent347.java
@@ -0,0 +1,64 @@
+package medium;
+
+import java.util.*;
+
+/**
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 示例 2:
+ * <p>
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+ * 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TopKFrequent347 {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> res = new ArrayList<>();
+        // 使用字典，统计每个元素出现的次数，元素为键，元素出现的次数为值
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        //桶排序
+        //将频率作为数组下标，对于出现频率不同的数字集合，存入对应的数组下标
+        List<Integer>[] list = new List[nums.length + 1];
+        for (int key : map.keySet()) {
+            // 获取出现的次数作为下标
+            int i = map.get(key);
+            if (list[i] == null) {
+                list[i] = new ArrayList<>();
+            }
+            list[i].add(key);
+        }
+
+        // 倒序遍历数组获取出现顺序从大到小的排列
+        for (int i = list.length - 1; i >= 0 && res.size() < k; i--) {
+            if (list[i] == null) continue;
+            res.addAll(list[i]);
+        }
+        return res;
+    }
+}
