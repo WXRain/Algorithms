cc689f8e86a9650f158363087c60363063620e71
S_rain <xiaomenxiaomen@qq.com>
2020-09-20 10:39:57 +0800

    daily update

---
+++ src/medium/Subsets78.java
@@ -0,0 +1,47 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ *
+ * 说明：解集不能包含重复的子集。
+ *
+ * 示例:
+ *
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subsets
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Subsets78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        res.add(temp);
+        if (nums == null || nums.length == 0) return new ArrayList<>(res);
+        for (int num : nums) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> list = new ArrayList<>(res.get(j));
+                list.add(num);
+                res.add(list);
+            }
+        }
+        return res;
+    }
+}
