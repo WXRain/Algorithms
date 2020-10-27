4221dc3cf2a820a342811a833fa583a148b1fccd
S_rain <xiaomenxiaomen@qq.com>
2020-10-27 10:01:56 +0800

    daily update

---
+++ src/medium/PreorderTraversal144.java
@@ -0,0 +1,36 @@
+package medium;
+
+import medium.RightSideView.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回它的 前序 遍历。
+ *
+ *  示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,2,3]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PreorderTraversal144 {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) return res;
+        res.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return res;
+    }
+}
