8f99714cb54d2423ae469fa7aa33f25afa552cc3
wxy <xiaomenxiaomen@qq.com>
2020-11-12 09:54:59 +0800

    daily update

---
+++ src/SortArrayByParityII922.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+ *
+ * 对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+ *
+ * 你可以返回任何满足上述条件的数组作为答案。
+ *
+ * 示例：
+ *
+ * 输入：[4,2,5,7]
+ * 输出：[4,5,2,7]
+ * 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+ *
+ * 提示：
+ *
+ * 2 <= A.length <= 20000
+ * A.length % 2 == 0
+ * 0 <= A[i] <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sort-array-by-parity-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SortArrayByParityII922 {
+    public int[] sortArrayByParityII(int[] A) {
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+        if (A == null || A.length == 0) return A;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) {
+                even.add(A[i]);
+            } else {
+                odd.add(A[i]);
+            }
+        }
+        int o = 0, e = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (i % 2 == 0) {
+                A[i] = even.get(e);
+                e++;
+            } else {
+                A[i] = odd.get(o);
+                o++;
+            }
+        }
+        return A;
+    }
+}
