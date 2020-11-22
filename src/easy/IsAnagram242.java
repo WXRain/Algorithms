6b802417efc0de17ee4cceb35bc33a5a82eff9c0
wxy <xiaomenxiaomen@qq.com>
2020-11-22 10:07:34 +0800

    daily update

---
+++ src/easy/IsAnagram242.java
@@ -0,0 +1,48 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsAnagram242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                int num = map.get(ch);
+                num++;
+                map.replace(ch, num);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!map.containsKey(ch)) return false;
+            int num = map.get(ch);
+            num--;
+            if (num == 0) {
+                map.remove(ch);
+            }
+            map.replace(ch, num);
+        }
+        return map.isEmpty();
+    }
+}
