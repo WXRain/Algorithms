58ad58da115f34b5d071e46fad985a6fe3242921
wxy <xiaomenxiaomen@qq.com>
2020-12-14 09:38:58 +0800

    daily update

---
+++ src/medium/GroupAnagrams49.java
@@ -0,0 +1,43 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * 说明：
+ *
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GroupAnagrams49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chs = s.toCharArray();
+            Arrays.sort(chs);
+            String key = String.valueOf(chs);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+    }
+}
