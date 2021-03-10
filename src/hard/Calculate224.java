a87f2b01c7ca429a1cdde00d5f02c390f9d4a296
S_rain <xiaomenxiaomen@qq.com>
2021-03-10 21:30:34 +0800

    daily update

---
+++ src/hard/Calculate224.java
@@ -0,0 +1,66 @@
+package hard;
+
+import java.util.Stack;
+
+/**
+ * 实现一个基本的计算器来计算一个简单的字符串表达式 s 的值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "1 + 1"
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：s = " 2-1 + 2 "
+ * 输出：3
+ * 示例 3：
+ *
+ * 输入：s = "(1+(4+5+2)-3)+(6+8)"
+ * 输出：23
+ *
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 3 * 105
+ * s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
+ * s 表示一个有效的表达式
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/basic-calculator
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Calculate224 {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        // sign 代表正负
+        int sign = 1, res = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                int cur = ch - '0';
+                while (i + 1 < length && Character.isDigit(s.charAt(i + 1)))
+                    cur = cur * 10 + s.charAt(++i) - '0';
+                res = res + sign * cur;
+            } else if (ch == '+') {
+                sign = 1;
+            } else if (ch == '-') {
+                sign = -1;
+            } else if (ch == '(') {
+                stack.push(res);
+                res = 0;
+                stack.push(sign);
+                sign = 1;
+            } else if (ch == ')') {
+                res = stack.pop() * res + stack.pop();
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        new Calculate224().calculate("(1+(4+5+2)-3)+(6+8)");
+    }
+}
