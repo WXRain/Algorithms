07415fc4fe6c66555bd528180b5a064d745c931d
wxy <xiaomenxiaomen@qq.com>
2021-04-01 21:36:12 +0800

    daily update

---
+++ src/easy/KthLargest_lcof54.java
@@ -0,0 +1,73 @@
+package easy;
+
+/**
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,1,4,null,2], k = 1
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * 输出: 4
+ * 示例 2:
+ *
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ *        5
+ *       / \
+ *      3   6
+ *     / \
+ *    2   4
+ *   /
+ *  1
+ * 输出: 4
+ *
+ *
+ * 限制：
+ *
+ * 1 ≤ k ≤ 二叉搜索树元素个数
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class KthLargest_lcof54 {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+    int total = 0;
+
+    public int kthLargest(TreeNode root, int k) {
+        return inOrder(root, k).val;
+    }
+
+    public TreeNode inOrder(TreeNode root, int k) {
+        if (root == null) return null;
+        TreeNode t = inOrder(root.right, k);
+        if (t != null) return t;
+        total++;
+        if (total == k) {
+            return root;
+        }
+        t = inOrder(root.left, k);
+        if (t != null) return t;
+        return null;
+    }
+}
