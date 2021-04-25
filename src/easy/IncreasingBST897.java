114bf78dc0914958069d891bdc06fbcb51cbc6bb
wxy <xiaomenxiaomen@qq.com>
2021-04-25 10:01:47 +0800

    daily update

---
+++ src/easy/IncreasingBST897.java
@@ -0,0 +1,60 @@
+package easy;
+
+/**
+ * 给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+ * 示例 2：
+ *
+ *
+ * 输入：root = [5,1,7]
+ * 输出：[1,null,5,null,7]
+ *
+ *
+ * 提示：
+ *
+ * 树中节点数的取值范围是 [1, 100]
+ * 0 <= Node.val <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/increasing-order-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IncreasingBST897 {
+
+    private class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(){
+
+        }
+        TreeNode(int val){
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private TreeNode res = new TreeNode(0);
+    private TreeNode cur = res;
+
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null) return null;
+        increasingBST(root.left);
+        TreeNode t = new TreeNode(root.val);
+        cur.right = t;
+        cur = cur.right;
+        increasingBST(root.right);
+        return res.right;
+    }
+}
