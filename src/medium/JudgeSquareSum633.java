e78e14a1a377ced080edfef4f87151afb2eef7c9
wxy <xiaomenxiaomen@qq.com>
2021-04-28 15:38:43 +0800

    daily update

---
+++ src/medium/JudgeSquareSum633.java
@@ -0,0 +1,54 @@
+package medium;
+
+/**
+ * 给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：c = 5
+ * 输出：true
+ * 解释：1 * 1 + 2 * 2 = 5
+ * 示例 2：
+ *
+ * 输入：c = 3
+ * 输出：false
+ * 示例 3：
+ *
+ * 输入：c = 4
+ * 输出：true
+ * 示例 4：
+ *
+ * 输入：c = 2
+ * 输出：true
+ * 示例 5：
+ *
+ * 输入：c = 1
+ * 输出：true
+ *
+ *
+ * 提示：
+ *
+ * 0 <= c <= 231 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-square-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class JudgeSquareSum633 {
+    public boolean judgeSquareSum(int c) {
+        int a = 0;
+        int b = (int) Math.sqrt(c);
+        while (a <= b) {
+            if (a * a + b * b < c) {
+                a++;
+            } else if (a * a + b * b > c) {
+                b--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
