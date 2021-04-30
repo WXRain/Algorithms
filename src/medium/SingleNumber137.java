db2f62a3f48b516b0215be5ca59087b358266948
WXRain <xiaomenxiaomen@qq.com>
2021-04-30 10:42:21 +0800

    daily update

---
+++ src/medium/SingleNumber137.java
@@ -0,0 +1,49 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,2,3,2]
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：nums = [0,1,0,1,0,1,99]
+ * 输出：99
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SingleNumber137 {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == 1)
+                return key;
+        }
+        return 0;
+    }
+}
